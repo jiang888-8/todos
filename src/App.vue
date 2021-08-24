5bf0a28cd720a44fb1468b3a40cc5415c1f507f7
jiangdou <1565133920@qq.com>
2021-08-24 11:13:52 +0800

    页面结构搭建完成

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
-  <div>
-    根页面
+  <div class="todoapp">
+    <todo-header></todo-header>
+    <todo-main></todo-main>
+    <todo-footer/>
   </div>
 </template>
 
 <script>
+import todoHeader from './components/todoHeader.vue'
+import todoMain from './components/todoMain.vue'
+import todoFooter from './components/todoFooter.vue'
 export default {
   data () {
     return {
 
     }
+  },
+
+  components: {
+    todoHeader,
+    todoMain,
+    todoFooter
   },
 
   created () {
