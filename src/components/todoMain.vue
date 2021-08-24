a9a42d16489364295bee2a06a13de819c7e0e802
jiangdou <1565133920@qq.com>
2021-08-24 16:34:39 +0800

    主体页面代码完成

--- src/components/todoMain.vue
+++ src/components/todoMain.vue
@@ -1,18 +1,24 @@
 <template>
   <ul class="todo-list">
     <!-- completed: 完成的类名 -->
-    <li class="completed" >
+    <li :class="{completed:item.isDone}" v-for="(item) in arr" :key='item.id'>
       <div class="view">
-        <input class="toggle" type="checkbox" />
-        <label>任务名</label>
-        <button class="destroy"></button>
+        <input class="toggle" type="checkbox" v-model="item.isDone" />
+        <label>{{item.name}}</label>
+        <button class="destroy" @click="del(item.id)"></button>
       </div>
     </li>
   </ul>
-  
 </template>
 
 <script>
 export default {
+  props: ['arr'],
+
+  methods: {
+    del (id) {
+      this.$emit('del', id)
+    }
+  }
 }
 </script>
