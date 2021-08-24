e541a0030abd913629300c21eebc6a012095b3f0
jiangdou <1565133920@qq.com>
2021-08-24 16:35:10 +0800

    尾部切换完成

--- src/components/todoFooter.vue
+++ src/components/todoFooter.vue
@@ -1,23 +1,44 @@
 <template>
   <footer class="footer">
-    <span class="todo-count">剩余<strong>数量值</strong></span>
+    <span class="todo-count">剩余<strong>{{residue}}</strong></span>
     <ul class="filters">
       <li>
-        <a class="selected" href="javascript:;" >全部</a>
+        <a :class="{selected: active === 'all'}" href="javascript:;" @click="$emit('setActive', 'all')">全部</a>
       </li>
       <li>
-        <a href="javascript:;">未完成</a>
+        <a :class="{selected: active === 'undone'}" href="javascript:;" @click="$emit('setActive', 'undone')">未完成</a>
       </li>
       <li>
-        <a href="javascript:;" >已完成</a>
+        <a :class="{selected: active === 'done'}" href="javascript:;" @click="$emit('setActive', 'done')">已完成</a>
       </li>
     </ul>
-    <button class="clear-completed" >清除已完成</button>
+    <button class="clear-completed" @click="del">清除已完成</button>
   </footer>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      done: []
+    }
+  },
 
+  props: ['arr', 'active'],
+
+  computed: {
+    residue () {
+      return this.arr.filter(item => !item.isDone).length
+    }
+  },
+
+  methods: {
+    del () {
+      const done = this.arr.filter(item => {
+        return !item.isDone
+      })
+      this.$emit('deled', done)
+    }
+  }
 }
 </script>
